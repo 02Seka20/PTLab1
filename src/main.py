835b8843cd36cc3a2c2178a205e17029df2f5ca3
02Seka20 <smabrosimov@mail.ru>
2024-10-16 00:44:52 +0300

    Second commit

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def main():
     reader = TextDataReader()
     students = reader.read(path)
     print("Students: ", students)
-    rating = CalcRating(students).calc()fdsdkjljsfdjkldgsklgsf;gdl,sl;gl;dksl;g'ldapvcxm;v;dzmkl;vdml;damvl;amklnalnajlnvdkmsbmmskmbl;fsml;mhl;sf
+    rating = CalcRating(students).calc()
     print("Rating: ", rating)
 
 
